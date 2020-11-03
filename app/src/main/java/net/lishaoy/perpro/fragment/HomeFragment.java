a8f1e8b38ec55580bacc18462753784f7679ad78
persilee <i@lishaoy.net>
2020-11-03 19:11:47 +0800

    🍇 add tab top demo

--- app/src/main/java/net/lishaoy/perpro/fragment/HomeFragment.java
+++ app/src/main/java/net/lishaoy/perpro/fragment/HomeFragment.java
@@ -1,12 +1,31 @@
 package net.lishaoy.perpro.fragment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import net.lishaoy.common.ui.PerBaseFragment;
 import net.lishaoy.perpro.R;
+import net.lishaoy.perpro.demo.PerTopLayoutActivity;
 
 public class HomeFragment extends PerBaseFragment {
 
     @Override
     public int getLayoutId() {
         return R.layout.fragment_home;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btn_tab_top).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), PerTopLayoutActivity.class));
+            }
+        });
     }
 }
