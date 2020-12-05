48f68d3a57f7b42c27779e3784325147ca34664c
persilee <i@lishaoy.net>
2020-12-05 21:22:23 +0800

    🍅 add notice list

---
+++ perlibrary/src/main/java/net/lishaoy/library/util/PerRes.kt
@@ -0,0 +1,32 @@
+package net.lishaoy.library.util
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+
+object PerRes {
+
+    fun getSting(@StringRes id: Int): String {
+        return context().getString(id)
+    }
+
+    fun getSting(@StringRes id: Int, vararg formatArgs: Any?): String {
+        return context().getString(id, *formatArgs)
+    }
+
+    fun getColor(@ColorRes id: Int): Int {
+        return ContextCompat.getColor(context(), id)
+    }
+
+    fun getDrawable(@DrawableRes id: Int): Drawable? {
+        return ContextCompat.getDrawable(context(), id)
+    }
+
+    private fun context(): Context {
+        return AppGlobals.get() as Context
+    }
+
+}
