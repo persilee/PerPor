2dad4ea0bb6c16cbf6c0d802d9546dfbee7d1c81
persilee <i@lishaoy.net>
2020-12-21 07:38:17 +0800

    🍅 add order model

---
+++ biz_order/src/main/java/net/lishaoy/biz_order/OrderActivity.kt
@@ -0,0 +1,11 @@
+package net.lishaoy.biz_order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
