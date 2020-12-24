f90f84dc9f16ee4eeb0235ce6720a68e3fe8b701
persilee <i@lishaoy.net>
2020-12-24 10:32:57 +0800

    🍐 add address list for layout files

---
+++ biz_order/src/main/java/net/lishaoy/biz_order/address/AddressListActivity.kt
@@ -0,0 +1,12 @@
+package net.lishaoy.biz_order.address
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.lishaoy.biz_order.R
+
+class AddressListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adress_list)
+    }
+}
