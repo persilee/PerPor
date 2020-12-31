ffc2f1f1cb2c5ac9c560f2a118d1dbe4f8629cd7
persilee <i@lishaoy.net>
2020-12-31 19:11:13 +0800

    🍈 add flutter module

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_module/page/favorite_page.dart';
+import 'package:flutter_module/page/recommend_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(FavoritePage()));
+
+@pragma('vm:entry-point')
+void recommend() => runApp(MyApp(RecommendPage()));
 
 class MyApp extends StatelessWidget {
+  final Widget page;
+
+  MyApp(this.page);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: page,
       ),
-      home: Container(),
     );
   }
 }
