d120ec95fb30c36773265818419858c8be77bec9
persilee <i@lishaoy.net>
2021-01-02 17:14:43 +0800

    🍉 add flutter for http files

--- flutter_module/lib/http/per_http.dart
+++ flutter_module/lib/http/per_http.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_module/http/request/base_request.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class PerHttp {
   PerHttp._();
@@ -12,13 +14,39 @@ class PerHttp {
     return _instance;
   }
 
-  Future  fire(BaseRequest request) async {
-    if(request.httpMethod() == HttpMethod.GET) {
+  Future fire(BaseRequest request) async {
+    if (request.httpMethod() == HttpMethod.GET) {
       return _doGet(request);
     }
   }
 
-  Future _doGet(BaseRequest request) {
+  Future _doGet(BaseRequest request) async {
+    var uri;
+    if (request.params.length != 0) {
+      uri = Uri.https(request.url(), request.path(), request.params);
+    } else {
+      uri = Uri.https(request.url(), request.path());
+    }
+    http.Response response;
+    var header = getHeaderParams();
+    response = await http.get(uri.toString(), headers: header);
+    Utf8Decoder utf8decoder = Utf8Decoder();
+    var result;
+    if (response.headers["content-type"].contains("/json")) {
+      result = json.decode(utf8decoder.convert(response.bodyBytes));
+    } else {
+      result = utf8.decode(response.bodyBytes);
+    }
+    if (response.statusCode == 200) {
+      if (result["code"] == 0) {
+        return result["data"];
+      } else {
+        throw Exception(result["msg"]);
+      }
+    }
+  }
 
+  getHeaderParams() {
+    return {"auth-token", "MTU5Mjg1MDg3NDcwNw11.26=="};
   }
 }
